c9880b95c5fc990c92558025a789c67dfeeaa495
Alex Lu <alexlu0831@gmail.com>
2022-06-08 22:28:24 +0800

    optimize the code with Eslint

--- src/App.js
+++ src/App.js
@@ -1,66 +1,47 @@
-// import {React ,useState} from "react";
+import React, { useState } from 'react';
+// eslint-disable-next-line import/no-named-as-default
+import Cart from './component/Cart';
+import Nav from './component/Nav';
+import Main from './component/Main';
 
+import data from './data';
 
-import Cart from "./component/Cart.js";
-import Nav from "./component/Nav.js";
-import Main from "./component/Main";
+function App() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
 
-import data from "./data";
-import { useState } from "react";
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(cartItems.map((x) => (x.id === product.id
+        ? { ...exist, qty: exist.qty + 1 } : x)));
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+      // console.log(...cartItems);
+    }
+  };
 
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(cartItems.map((x) => (x.id === product.id
+        ? { ...exist, qty: exist.qty - 1 } : x)));
+    }
+  };
 
-const App=()=>{
+  return (
 
-const {products} = data;
-const [cartItems ,setCartItems] = useState([]);
+    <div>
 
-const onAdd = (product) => {
-  const exist =  cartItems.find(x => x.id === product.id);
-  if(exist){
-    setCartItems(cartItems.map(x => x.id === product.id ? {...exist , qty: exist.qty +1} : x))
- 
+      <Nav />
 
+      <Cart cartItems={cartItems} onAdd={onAdd} onRemove={onRemove} />
+      <Main products={products} onAdd={onAdd} />
 
-} else{
-  setCartItems([...cartItems, {...product , qty:1}]);
-  console.log(...cartItems);
-};
-
-
-
-
-};
-
-const onRemove = (product) => {
-  const exist =  cartItems.find(x => x.id === product.id);
-  if (exist.qty === 1){
-    setCartItems(cartItems.filter((x)=> x.id !== product.id));
-
-  } else{
-    setCartItems(cartItems.map(x => x.id === product.id ? {...exist , qty: exist.qty  - 1} : x))
-  }
-
-
-};
-
-
-return(
-
-
- 
-  <div>
-
-<Nav></Nav>
-
-<Cart cartItems={cartItems} onAdd={onAdd}  onRemove={onRemove}></Cart>
-<Main products={products} onAdd={onAdd} ></Main>
-
-
-
-
-  </div>
-);
-
+    </div>
+  );
 }
 
 export default App;
